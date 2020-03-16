d25ef6a5206b4cbcb00453640a1a4c742cdd628a
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-03-16 10:09:00 +0700

    Updating Context Data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import BookList from './components/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar/>
-        <BookList/>
+        <Navbar />
+        <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
